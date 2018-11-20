dda737e61987e0b550a5908f10a192d811a88fce
Lucca Kaiser <luccakaiser@gmail.com>
2018-11-20 20:47:39 +0100

    fix authentication for :city and new, add basic home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/fields/city/:city' => "fields#city"
+  get '/fields/city/:city' => "fields#city", as: :city
   resources :fields
   resources :bookings, except: :delete
 end
