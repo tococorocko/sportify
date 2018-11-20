13beb453370b8d8cb3fc05c3cb5515163f8b444b
Daniel Trujillo <ganimedes@firemail.cc>
2018-11-20 13:34:47 +0100

    added image upload

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_19_174811) do
+ActiveRecord::Schema.define(version: 2018_11_20_101212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2018_11_19_174811) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "photo"
     t.index ["user_id"], name: "index_fields_on_user_id"
   end
 
